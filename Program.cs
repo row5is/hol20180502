456832855ef6f3b0ec7e08af4a7be87cad81871f
Rob Keiser <Rob.Keiser@coxautoinc.com>
2018-05-02 08:52:40 -0400

    updated SQLite database including Seed Data

--- Program.cs
+++ Program.cs
@@ -5,17 +5,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie
 {
     public class Program
     {
         public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+{
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MovieContext>();
+                    // requires using Microsoft.EntityFrameworkCore;
+                    context.Database.Migrate();
+                    // Requires using RazorPagesMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
+
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
